80f4b9923f4c35ce5877da988a3754f980e667ed
ovprakash-viraj <ovprakash@gmail.com>
2026-09-22 01:10:16 +0530

    Publish D365 project margin article with cover and reader feedback

---
+++ d365-project-margin/comments.js
@@ -0,0 +1,7 @@
+document.getElementById('comment-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const comment = document.getElementById('comment').value.trim();
+  if (!comment) return;
+  window.location.href = 'mailto:ovprakash@gmail.com?subject=' + encodeURIComponent('Comment: D365 project margin article') + '&body=' + encodeURIComponent(comment + '\n\nArticle: https://www.virajprakash365.com/d365-project-margin/');
+});
+
