fa0ab960679b46ddecf5842b3751c5f11d21bc46
ovprakash-viraj <ovprakash@gmail.com>
2026-09-22 01:29:42 +0530

    Publish Project Operations subcontractor cost reconciliation article

---
+++ project-operations-subcontractor-costs/comments.js
@@ -0,0 +1,8 @@
+document.getElementById('comment-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const comment = document.getElementById('comment').value.trim();
+  if (!comment) return;
+  window.location.href = 'mailto:ovprakash@gmail.com?subject=' + encodeURIComponent('Comment: Project Operations subcontractor costs') + '&body=' + encodeURIComponent(comment + '\n\nArticle: https://www.virajprakash365.com/project-operations-subcontractor-costs/');
+});
+
+
